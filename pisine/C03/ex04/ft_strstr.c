fcd866d41aa2dcac052b20ad684f0a9fccaf41d7
Harim Seong <hseong@c2r3s4.42seoul.kr>
2021-10-12 17:22:19 +0900

    C03 complete

--- pisine/C03/ex04/ft_strstr.c
+++ pisine/C03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: hseong <hseong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 23:24:32 by hseong            #+#    #+#             */
-/*   Updated: 2021/10/11 23:37:11 by hseong           ###   ########.fr       */
+/*   Updated: 2021/10/12 17:17:07 by hseong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strstr(char *str, char *to_find)
 	char	*itr;
 
 	if (*to_find == 0)
-		return str;
+		return (str);
 	ret = 0;
 	while (*str != 0)
 	{
@@ -30,9 +30,9 @@ char	*ft_strstr(char *str, char *to_find)
 				++itr;
 			}
 			if (*itr == 0)
-				break;
+				break ;
 			ret = 0;
-			continue;
+			continue ;
 		}
 		++str;
 	}
--- pisine/C03/ex05/ft_strlcat.c
+++ pisine/C03/ex05/ft_strlcat.c
@@ -1,28 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hseong <hseong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 17:21:20 by hseong            #+#    #+#             */
+/*   Updated: 2021/10/12 17:21:22 by hseong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	idx;
 	unsigned int	ret;
 
+	ret = 0;
+	while (src[ret] != 0)
+		++ret;
 	idx = 0;
 	while (idx < size && *dest != 0)
 	{
-		++dest;
 		++idx;
+		++ret;
+		++dest;
 	}
-	ret = idx;
+	if (size == 0)
+		return (ret);
 	while (idx < size - 1 && *src != 0)
 	{
 		*dest = *src;
-		++ret;
 		++idx;
 		++dest;
 		++src;
-	}
-	if (
-	while (*src != 0)
-	{
-		++src;
-		++ret;
+		*dest = 0;
 	}
 	return (ret);
 }
