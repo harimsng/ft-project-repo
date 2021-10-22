9abfa13f87b98c75adb33086461b0b52e101fc61
Harim <halim9512@naver.com>
2021-10-22 21:55:18 +0900

    mylib

---
+++ mylib/includes/ft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hseong <hseong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 14:43:06 by hseong            #+#    #+#             */
+/*   Updated: 2021/10/22 21:08:29 by hseong           ###   ########.kr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+void	ft_putnbr(int num);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ mylib/srcs/ft_putnbr.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+
+int check_num(int *num);
+
+void	ft_putnbr(int num)
+{
+	char	put;
+	int		denom;
+
+	if (check_num(&num))
+		return ;
+	denom = 1000000000;
+	while (num / denom == 0)
+		denom /= 10;
+	while (denom > 0)
+	{
+		put = num / denom + '0';
+		write(1, &put, 1);
+		num = num % denom;
+		denom /= 10;
+	}
+}
+
+int	check_num(int *num)
+{
+	if (*num == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return (1);		
+	}
+	else if (*num == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	else if (*num < 0)
+	{
+		*num *= -1;
+		return (0);
+	}
+	return (0);
+}
