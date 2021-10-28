f36d96bbc99e0951c9977185d3e28f33d5ca8e8e
Harim Seong <hseong@c1r3s7.42seoul.kr>
2021-10-28 16:54:38 +0900

    C10 ex03 in progress

---
+++ pisine/C10/ex03/includes/ft_hexd.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_hexd.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hseong <hseong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 16:36:17 by hseong            #+#    #+#             */
+/*   Updated: 2021/10/28 16:51:11 by hseong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_HEXD_H
+# define FT_HEXD_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+void	ft_putstr(char *str);
+void	ft_puterr(char *str);
+
+#ifndef
---
+++ pisine/C10/ex03/srcs/ft_hexdump.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_hexdump.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hseong <hseong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 16:37:56 by hseong            #+#    #+#             */
+/*   Updated: 2021/10/28 16:54:14 by hseong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_hexd.h"
+
+// $> hexdump nonexist
+// hexdump: nonexist: No such file or directory
+// hexdump: nonexist: Bad file descriptor
+//
+// $> hexdump .
+// hexdump: .: Is a directory
+//
+// $> hexdump (-C)
+// > (stdin)
+//
+// $< hexdump file1 nonexist file2 ...
+// ..010 ....
+// ..020 ....
+// ...
+// ..0a0 ....
+// hexdump: nonexist: No such file or directory
+// ..0b0 ....
+// ...
+
+static const char	*prog_name = "hexdump: ";
+
+int					print_input(char *filename, int option);
+void				print_error(char *filename);
+
+int	main(int argc, char **argv)
+{
+	int				option;
+
+	option = (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'C' && argv[1][2] == 0);
+	if (argc == 1 || (argc == 2 && option))
+	{
+		print_input(0, option);
+		return (0);
+	}
+	while (--argc > 0)
+	{
+		if (print_input(*argv, option))
+			print_error(*argv);
+		++argv;
+	}
+	return (0);
+}
+
+void	print_error(char *filename)
+{
+	ft_puterr(prog_name);
+	ft_puterr(filename);
+	ft_puterr(": ");
+	ft_puterr(strerr(errno));
+	ft_puterr("\n");
+}
+
+int	print_input(char *filename, int option)
+{
+
+}
