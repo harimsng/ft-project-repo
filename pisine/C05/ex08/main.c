2c28480a43609a1c923cce65227e023fe86c6a28
hseong <hseong@student.42seoul.kr>
2021-11-28 02:19:28 +0900

    ten queens' puzzle: time count added.

--- pisine/C05/ex08/main.c
+++ pisine/C05/ex08/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <time.h>
 
 int		ft_ten_queens_puzzle(void);
 
 int	main(void)
 {
+	clock_t	start, end;
+	double	time;
+
+	start = clock();
 	printf("%d\n", ft_ten_queens_puzzle());
+	end = clock();
+	time = (double)(end - start) / (CLOCKS_PER_SEC);
+	printf("time taken : %f\n", time);
 	return (0);
 }
