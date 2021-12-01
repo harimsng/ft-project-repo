ac659819b1e5ca3ebdf23fe4393cc7778372e17e
hseong <hseong@student.42seoul.kr>
2021-12-01 20:50:27 +0900

    gnl: second revision for efficient memory usage and high performance in progress.

--- core1/gnl/get_next_line.c
+++ core1/gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hseong <hseong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 22:35:55 by hseong            #+#    #+#             */
-/*   Updated: 2021/11/29 20:04:18 by hseong           ###   ########.fr       */
+/*   Updated: 2021/11/30 22:31:06 by hseong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,16 @@
 int		gnl_load_buf(t_buf *fd_buf, t_buf *buf, int fd);
 int		gnl_save_buf(t_buf *fd_buf, t_buf *buf);
 int		gnl_append(t_line *line, t_buf *buf);
-void	gnl_reset_buf(t_buf *buf);
 
-// maximum number of opened fd + stdin = 257
 char	*get_next_line(int fd)
 {
-	static t_buf	fd_buf[257];
+	static t_buf	fd_buf[512];
 	t_buf			buf;
 	t_line			line;
 
 	if (fd < 0 || BUFFER_SIZE < 1 || gnl_load_buf(fd_buf, &buf, fd))
 		return (NULL);
-	line = (t_line){128, 0, ft_calloc(128, sizeof(char))};
+	line = (t_line){128, 0, malloc(sizeof(char) * 128)};
 	if (!line.str)
 		return (NULL);
 	while (gnl_append(&line, &buf))
@@ -41,22 +39,16 @@ char	*get_next_line(int fd)
 		free(line.str);
 		line.str = NULL;
 	}
-	gnl_reset_buf(&buf);
+	free(buf.str);
 	return ((char *)line.str);
 }
-//	buf->size == -1
-//		read() error
-//	line.size == 0
-//		only EOF has been read(nothing left in the fd)
 
-//		check whether preserved fd_buf exists or not.
-// 		if it exists, move its contents to buf.
 int	gnl_load_buf(t_buf *fd_buf, t_buf *buf, int fd)
 {
 	int				idx;
 
 	fd += INT_MAX * (!fd);
-	*buf = (t_buf){fd, 0, ft_calloc(BUFFER_SIZE, sizeof(char))};
+	*buf = (t_buf){fd, 0, malloc(sizeof(char) * BUFFER_SIZE)}; 
 	if (!(buf->str))
 		return (1);
 	idx = 0;
@@ -66,7 +58,9 @@ int	gnl_load_buf(t_buf *fd_buf, t_buf *buf, int fd)
 		return (0);
 	ft_memcpy(buf->str, fd_buf[idx].str, fd_buf[idx].size);
 	buf->size = fd_buf[idx].size;
-	gnl_reset_buf(fd_buf + idx);
+	free(fd_buf->str);
+	fd_buf->fd = 0;
+	fd_buf->str = NULL;
 	return (0);
 }
 
@@ -81,13 +75,11 @@ int	gnl_save_buf(t_buf *fd_buf, t_buf *buf)
 		return (1);
 	fd_buf[idx].fd = buf->fd;
 	fd_buf[idx].size = buf->size;
-	fd_buf[idx].str = ft_calloc(buf->size, sizeof(char));
+	fd_buf[idx].str = malloc(sizeof(char) * buf->size);
 	ft_memcpy(fd_buf[idx].str, buf->str, buf->size);	
 	return (0);
 }
 
-//		if expected line size is bigger than capacity,
-//		allocate new array with proper capacity and memcpy original contents.
 int	gnl_append(t_line *line, t_buf *buf)
 {
 	unsigned char	*ptr;
@@ -95,7 +87,7 @@ int	gnl_append(t_line *line, t_buf *buf)
 
 	if (line->size + buf->size >= line->cap)
 	{
-		line->cap = line->size + buf->size + 1;
+		line->cap = line->size + buf->size + 128;
 		ptr = malloc(sizeof(char) * (line->cap));
 		if (!ptr)
 			return (line->size = 0);
@@ -112,12 +104,4 @@ int	gnl_append(t_line *line, t_buf *buf)
 	line->str[line->size] = 0;
 	buf->size -= idx;
 	return (!line->size || line->str[line->size - 1] != '\n');
-}
-
-void gnl_reset_buf(t_buf *buf)
-{
-	free(buf->str);
-	buf->str = NULL;
-	buf->fd = 0;
-	buf->size = 0;
 }
