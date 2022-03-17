2d1ab88fb0e88d04cfa2783cc9d0021ae5435b3b
Harim Seong <hseong@c5r1s4.42seoul.kr>
2022-03-17 16:29:36 +0900

    pipex: norminette fix

--- core2/pipex/src/pipex_heredoc.c
+++ core2/pipex/src/pipex_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: hseong <hseong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 21:29:39 by hseong            #+#    #+#             */
-/*   Updated: 2022/03/17 16:22:55 by hseong           ###   ########.fr       */
+/*   Updated: 2022/03/17 16:29:01 by hseong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ void	pipex_heredoc(int *in_fd, char *limiter)
 	int		pipe_fd[2];
 	char	*bufptr;
 	size_t	lim_len;
-	
+
 	pipe(pipe_fd);
 	*in_fd = pipe_fd[0];
 	lim_len = ft_strlen(limiter);
 	write(1, "> ", 2);
 	bufptr = get_next_line(0);
 	while (bufptr == NULL
-			|| ft_strncmp(bufptr, limiter, lim_len) != 0
-			|| bufptr[lim_len] != '\n')
+		|| ft_strncmp(bufptr, limiter, lim_len) != 0
+		|| bufptr[lim_len] != '\n')
 	{
 		write(pipe_fd[1], bufptr, ft_strlen(bufptr));
 		if (bufptr != NULL)
