af1b2eeb1871dc3734d7c3750a53eeb2c4cb89fb
Harim Seong <hseong@c4r5s1.42seoul.kr>
2022-03-22 16:12:33 +0900

    update

--- core2/push_swap/testdir/test.sh
+++ core2/push_swap/testdir/test.sh
@@ -10,6 +10,7 @@ INPUT="`cat testcase.txt`"
 	../push_swap $INPUT > instruction_temp.txt
 	echo "number of operations"
 	cat instruction_temp.txt | wc -l
+	./checker_Mac $INPUT < instruction_temp.txt
 	./checker_linux $INPUT < instruction_temp.txt
 	./checker $INPUT < instruction_temp.txt
 fi
