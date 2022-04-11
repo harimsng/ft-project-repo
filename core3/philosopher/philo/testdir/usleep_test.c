5e03a069d7ae7536b43f4713bf3db59686c95ea7
hseong <hseong@student.42seoul.kr>
2022-04-11 19:48:24 +0900

    update

--- core3/philosopher/philo/testdir/usleep_test.c
+++ core3/philosopher/philo/testdir/usleep_test.c
@@ -8,15 +8,17 @@ int	main(void)
 	int		measure;
 	struct timeval get_time;
 
-	time = 1000000;
-	printf("expected usleep(%d) = %lf ms = %lf s\n",
-			time, (double)time / 1000, (double)time / 1000000);
-	gettimeofday(&get_time, NULL);
-	measure = get_time.tv_sec * 1000000 + get_time.tv_usec;
-	usleep(time);
-	gettimeofday(&get_time, NULL);
-	measure = get_time.tv_sec * 1000000 + get_time.tv_usec - measure;
-	printf("measured usleep(%d) = %lf ms = %lf s\n",
-			measure, (double)measure / 1000, (double)measure / 1000000);
+	for (time = 1000; time <= 1000000; time *= 10)
+	{
+		gettimeofday(&get_time, NULL);
+		measure = get_time.tv_sec * 1000000 + get_time.tv_usec;
+		usleep(time);
+		gettimeofday(&get_time, NULL);
+		measure = get_time.tv_sec * 1000000 + get_time.tv_usec - measure;
+		printf("expected usleep(%d) = %lf ms = %lf s\n",
+				time, (double)time / 1000, (double)time / 1000000);
+		printf("measured usleep(%d) = %lf ms = %lf s\n",
+				time, (double)measure / 1000, (double)measure / 1000000);
+	}
 	return (0);
 }
