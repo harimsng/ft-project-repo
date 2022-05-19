ff4aec3250e1f5a86e438806f40b2d9a6cdd2d3e
hseong <hseong@student.42seoul.kr>
2022-05-20 02:46:03 +0900

    cub3d: added linux keymap. fixed max distance output.

--- core4/cub3d/src/c3d_draw_scene.c
+++ core4/cub3d/src/c3d_draw_scene.c
@@ -6,7 +6,7 @@
 /*   By: hseong <hseong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 20:39:33 by hseong            #+#    #+#             */
-/*   Updated: 2022/05/18 22:27:22 by hseong           ###   ########.fr       */
+/*   Updated: 2022/05/19 18:05:54 by hseong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,16 @@ c3d_draw_scene(t_mlx_info *mlx_info, t_camera *camera, int count, double dist)
 	int		color;
 
 	(void)camera;
-	map = mlx_info->img_elem->img_buf;
-	len = GRID_WIDTH * VP_DIST / dist * 2;
-	idx = (SCREEN_HEIGHT - len) / 2;
-	if (idx + len >= SCREEN_HEIGHT)
+	if ((int)dist >= MAX_DIST)
 		return ;
+	map = mlx_info->img_elem->img_buf;
+	len = 2 * GRID_WIDTH * VP_DIST / dist;
+	idx = (SCREEN_HEIGHT - len) >> 1;
+	if (idx + len >= SCREEN_HEIGHT)
+	{
+		idx = 0;
+		len = SCREEN_HEIGHT;
+	}
 	hor_px = mlx_info->img_elem->hor_px;
 	color = 0xFF * (1.0 - 2 * dist / (MAX_DIST + dist));
 	color = (color << 16) + (color << 8) + color;
